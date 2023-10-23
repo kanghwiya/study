4cedae5dab22d8e1e1628bd53673630db83b0972
kanghwiya <fiminmy@gmail.com>
2023-10-23 17:40:53 +0900

    1023 js

---
+++ javascript/js/04.js
@@ -0,0 +1,28 @@
+// console.log("반갑습니다. JS파일입니다.");
+
+// ----------------------
+// 변수 (var, let, const)
+// ----------------------
+// var : 중복 선언 가능, 재할당 가능, 함수레벨 스코프를 가짐
+// console 은 객체
+// var u_name = "홍길동";
+// console.log(u_name);
+// var u_name = "갑순이"; //var를 제거하면 재할당 : ?
+// console.log(u_name);
+
+// let : 중복 선언 불가능, 재할당 가능, 블록레벨 스코프
+// let u_name = "홍길동";
+// console.log(u_name);
+// let u_name = "갑순이"; 
+// console.log(u_name);
+
+// 재할당
+// let u_name = "홍길동";
+// console.log(u_name);
+// u_name = "갑순이"; 
+// console.log(u_name);
+
+// const : 중복 선언 불가능, 재할당 불가능, 블록레벨 스코프
+const AGE = 19;
+// AGE = 20;
+console.log(AGE);
